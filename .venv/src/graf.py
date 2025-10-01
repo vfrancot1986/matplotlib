ff4f48ab00ee9c5f690cc684898ddc4dd9728e92
vfrancot1986 <vinicius.ts.franco@gmail.com>
2025-09-30 21:59:49 -0300

    primeiro-grafico

---
+++ .venv/src/graf.py
@@ -0,0 +1,10 @@
+# importando o módulo matplotlib
+import matplotlib.pyplot as plt
+
+#listas de valores X e Y
+x_coords = [0, 1, 2, 3, 4]
+y_coords = [0, 3, 1, 5, 2]
+
+plt.plot(x_coords,y_coords)
+
+plt.show()
